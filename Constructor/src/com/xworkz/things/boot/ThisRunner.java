dcbee69e610ee8e7267faf21619b251c1f8d72f9
unknown <chaithram.xworksz@gmail.com>
2022-10-18 18:49:39 +0530

    git

--- Constructor/src/com/xworkz/things/boot/ThisRunner.java
+++ Constructor/src/com/xworkz/things/boot/ThisRunner.java
@@ -1,5 +1,5 @@
 package com.xworkz.things.boot;
-import com.xworkz.things.Tractor;
+import com.xworkz.things.*;
 
 public class TractorRunner {
 
@@ -14,7 +14,26 @@ public class TractorRunner {
 		System.out.println(obj.hp);
 		System.out.println(obj.model);
 		
+		System.out.println("Enter the delails of TaxtileShopOwner");
+		TextileShop shop=new TextileShop("Rajendra","Suguna",9856420176,1,245);
+		System.out.println(shop.ownerName);
+		System.out.println(shop.ownerWifeName);
+		System.out.println(shop.ownerDaughterName);
+		System.out.println(shop.ownerDaughterNumber);
+		System.out.println(shop.ownersNoOfWifes);
+		System.out.println(shop.shopNo);
+		System.out.println(shop.contactNo);
 		
+		System.out.println("Enter the details of Chain");
+		Chain ref=new Chain(10,"belt","dog",250);
+		System.out.println(ref.type);
+		System.out.println(ref.price);
+		System.out.println(ref.length);
+		System.out.println(ref.material);
+		System.out.println(ref.weight);
+		System.out.println(ref.stolen);
+		System.out.println(ref.usedFor);
+		System.out.println(ref.fresh);
 	}
 
 }
