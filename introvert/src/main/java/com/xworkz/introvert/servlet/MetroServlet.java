c62eaa2ba03c17e6cec72e26e60af6231298c4c3
unknown <chaithram.xworksz@gmail.com>
2023-01-24 23:21:48 +0530

    post,get

---
+++ introvert/src/main/java/com/xworkz/introvert/servlet/MetroServlet.java
@@ -0,0 +1,66 @@
+package com.xworkz.introvert.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(loadOnStartup = 1,urlPatterns = "/train")
+public class MetroServlet extends HttpServlet
+{
+	public MetroServlet() 
+	{
+		System.out.println("Created the metro.....");
+	}
+	@Override
+	public void init() throws ServletException 
+	{
+		System.out.println("init metro");
+	}
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
+	{
+		System.out.println("Running the doGet metro");
+	}
+	@Override
+	protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
+	{
+		System.out.println("Running the metro option");
+		String data = "displaying data as get";
+		PrintWriter writer = resp.getWriter();
+		writer.print(data);
+		resp.setContentType("text/plain");
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
+	{
+		System.out.println("running the doPost in metro");
+		String ref = "Create the spring in the post method";
+		PrintWriter writer = resp.getWriter();
+		writer.print(5);
+		resp.setContentType("text/plain");
+	}
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
+	{
+		System.out.println("running the doDelete in metro");
+	}
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
+	{
+		System.out.println("running the doPut in metro...");
+	}
+	@Override
+	protected void doTrace(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
+	{
+		System.out.println("running the doTrace in metro" );
+	}
+	@Override
+	protected void doHead(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
+	{
+		System.out.println("Running the doHead in metro");
+	}
+}
