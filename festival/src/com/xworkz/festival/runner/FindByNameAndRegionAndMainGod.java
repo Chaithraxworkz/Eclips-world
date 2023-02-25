bdcf62fd989ffe7c54d1f58b6576b21b760c7a9e
unknown <chaithram.xworksz@gmail.com>
2023-02-25 10:15:36 +0530

    persist

---
+++ festival/src/com/xworkz/festival/runner/FindByNameAndRegionAndMainGod.java
@@ -0,0 +1,26 @@
+package com.xworkz.festival.runner;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+
+public class FindByNameAndRegionAndMainGod 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Runnind in FindByNameAndRegionAndMainGod ");
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("com.xworkz");
+		EntityManager manager = factory.createEntityManager();
+		Query query = manager.createNamedQuery("findByNameAndRegionAndMainGod");
+		query.setParameter("nam", "Lohri");
+		query.setParameter("reg", "Andhra pradesh");
+		query.setParameter("main", "Lohri");
+		Object obj = query.getSingleResult();
+		FestivalEntity entity = (FestivalEntity) obj;
+		System.out.println(entity);
+	}
+
+}
